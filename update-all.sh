8ad70d581e4441941a659a8930392bfcdfcdd580
Leonorm56 <kshakul119@gmail.com>
2026-05-10 15:07:03 +0300

    Clean up remaining purrfect branding

--- update-all.sh
+++ update-all.sh
@@ -3,14 +3,12 @@
 set -e
  
 FOLDERS=(
-  "$HOME/purrfect-farmer-one"
-  "$HOME/purrfect-farmer-two"
-  "$HOME/purrfect-farmer-three"
+  "$HOME/nilecloud"
 )
  
 for FOLDER in "${FOLDERS[@]}"; do
   echo ">>> $FOLDER"
   cd "$FOLDER"
-  bash apps/purrfect-fly/update.sh
+  bash update.sh
   echo ""
 done
